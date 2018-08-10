12ab680461fcde5bce3cba9e1cf20bb51b6b3f55
Stanley Shyiko <stanley.shyiko@gmail.com>
2018-08-10 10:54:17 -0700

    0.9.1

--- kubesec.rb
+++ kubesec.rb
@@ -1,5 +1,5 @@
 class Kubesec < Formula
-  desc "A tool for encrypting and decrypting k8s secrets with AWS KMS"
+  desc "Secure Secret management for Kubernetes (with gpg, Google Cloud KMS and AWS KMS backends)"
   homepage "https://github.com/shyiko/kubesec"
   url "https://github.com/shyiko/kubesec/archive/0.9.1.tar.gz"
   sha256 "2e00e8b08e5b6b61c400d0b97b5c0ef3b5945adb31a8d4df6ec8f5c5091d7f5a"
@@ -15,12 +15,12 @@ class Kubesec < Formula
     (buildpath/"src/github.com/shyiko/kubesec").install buildpath.children
     cd "src/github.com/shyiko/kubesec" do
       system "make", "fetch"
-      system "make", "build"      
+      system "make", "build"
       bin.install "kubesec" => build.with?("short-name") ? "ksec" : "kubesec"
     end
   end
 
   test do
-    system bin/"kubesec"
+    system bin/"kubesec", "--version"
   end
 end
